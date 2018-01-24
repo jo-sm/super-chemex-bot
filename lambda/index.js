64799bbfe90319775e0b03ff97cee1bfa4a3eb59
Joshua Smock <github@joshuasmock.com>
2018-01-24 03:58:34 +0100

    Migrate handler to use utilities and generator

--- lambda/index.js
+++ lambda/index.js
@@ -1,46 +1,61 @@
-'use strict';
+const contentfulManagement = require('contentful-management');
+const contentful = require('contentful');
 
-const contentful = require('./contentful')
-const slack = require('./slack')
-const deviceID = 'G030MD025452LHCJ';
+const { run, getAllEntries, updateStats, getConfigurationFor, getMessagesFor } = require('./utils');
+const { send } = require('./slack');
 
 /*
+Event from Amazon IoT button click:
+
 { 
   serialNumber: 'G030MD025452LHCJ',
   batteryVoltage: '1737mV',
-  clickType: 'SINGLE'
+  clickType: 'SINGLE' // Could also be 'DOUBLE'
 }
  */
 
-exports.handler = (event) => {
+process.on('unhandledRejection', error => {
+  // Will print "unhandledRejection err is not defined"
+  console.log('unhandledRejection');
+  console.error(error);
+});
+
+exports.handler = (event, context, cb) => {
+  // Env variables
+  const deliveryToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+  const managementToken = process.env.CONTENTFUL_MANAGEMENT_ACCESS_TOKEN;
+  const spaceId = process.env.CONTENTFUL_SPACE_ID;
+  const slackToken = process.env.SLACK_API_TOKEN
+
+  // Invocation variables
   const start = new Date();
+  const batteryVoltage = parseInt(event.batteryVoltage, 10);
+  const {
+    deviceSerialNumber,
+    debug
+  } = event;
 
-  contentful.withEntries().then(function(def) {
-    return def.updateStats(start).then(function(num) {
-      return def.getConfiguration().then(function(config) {
-        return [num, config];
-      });
-    }).then(function(values) {
-      const num = values[0];
-      const config = values[1];
+  // Contentful management client
+  const managementClient = contentfulManagement.createClient({
+    space: spaceId,
+    accessToken: managementToken
+  });
 
-      return def.getMessagesFor(num).then(function(messages) {
-        return [config, messages];
-      });
-    }).then(function(values) {
-      const { slackChannel } = values[0];
-      const messages = values[1];
+  run(function* () {
+    const entries = yield getAllEntries(managementClient);
+    const currentUsage = yield updateStats(managementClient, entries);
+    const messages = yield getMessagesFor(entries, currentUsage);
+    const config = yield getConfigurationFor(entries, deviceSerialNumber);
 
-      const randomMessageIdx = Math.floor(Math.random() * messages.length);
+    const { slackChannel } = config;
+    const randomMessageIdx = Math.floor(Math.random() * messages.length);
+    const message = messages[randomMessageIdx];
 
-      const message = messages[randomMessageIdx];
-
-      return slack.send(slackChannel, message);
-    }).then(function(resp) {
-      console.info('Message sent', resp);
-      console.info(`Total time: ${(new Date()) - start}`);
-    }).catch(function(error) {
-      console.error(error);
-    });
+    return send(slackToken, slackChannel, message);
+  }).then(function(sentMessage) {
+    console.info('Message sent: ', sentMessage);
+    console.info(`Total time: ${(new Date()) - start}`);
+  }).catch(function(e) {
+    console.error(e);
   });
 }
--- lambda/slack.js
+++ lambda/slack.js
@@ -1,12 +1,9 @@
 'use strict';
 
-const WebClient = require('@slack/client').WebClient
+const WebClient = require('@slack/client').WebClient;
 
-const token = process.env.SLACK_API_TOKEN
-const web = new WebClient(token)
-
-function send(channel, message) {
-  return web.chat.postMessage(channel, message)
+function send(token, channel, message) {
+  return (new WebClient(token)).chat.postMessage(channel, message)
 }
 
 module.exports = {
